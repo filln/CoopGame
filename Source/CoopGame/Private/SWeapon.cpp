15827d5986001142f07562085d2f75d020822c79
filln <ulv0710@gmail.com>
2019-09-16 13:26:29 +0300

    Add comments

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -11,6 +11,7 @@
 #include "TimerManager.h"
 #include "Net/UnrealNetwork.h"
 
+//Консольная команда для отрисовки трейсинга (дебаг-линии)
 static int32 DebugWeaponDrawing = 0;
 FAutoConsoleVariableRef CVARDebugWeaponDriwing(
 	TEXT("COOP.DebugWeaponDrawing"), 
@@ -21,7 +22,10 @@ FAutoConsoleVariableRef CVARDebugWeaponDriwing(
 // Sets default values
 ASWeapon::ASWeapon()
 {
+	//Создать меш
 	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+
+	//Назначить рутом
 	RootComponent = MeshComp;
 
 	MuzzleSocketName = "MuzzleSocket";
@@ -29,74 +33,110 @@ ASWeapon::ASWeapon()
 
 	BaseDamage = 20.0f;
 	RateOfFire = 600;
+	LenghtTracing = 10000;
 	
 	//Объект можно передавать по сети
 	SetReplicates(true);
 
-	//Частота обновления сети - от этого зависит скорость передачи данных
+	//Как часто этот объект будет реплицироваться
 	NetUpdateFrequency = 66.0f;
+
+	//Как часто этот объект будет реплицироваться при нечастом изменении реплицируемых свойств
 	MinNetUpdateFrequency = 33.0f;
 }
 
 void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+
 	TimeBetweenShots = 60 / RateOfFire;
 }
 
 
 void ASWeapon::Fire()
 {
-
+	//Если выполняется на клиенте
 	if (Role < ROLE_Authority)
 	{
+		//Выполнить на сервере
 		ServerFire();
 	}
 
+	//Взять овнера - владельца оружия
 	AActor* MyOwner = GetOwner();
 	if (MyOwner == nullptr)
 	{
 		return;
 	}
 	   
+	//Структура для данных, получаемых при трейсинге
 	FHitResult Hit;
 
+	//точка "глаз" овнера
 	FVector EyeStart;
+
+	//Направление взгляда
 	FRotator EyeRotation;
+
+	//Определить вектор и ротатор EyeStart и EyeRotation
 	MyOwner->GetActorEyesViewPoint(EyeStart, EyeRotation);
 
+	//Единичный вектор, направленный в сторону ротатора EyeRotation
 	FVector ShotDirection = EyeRotation.Vector();
-	FVector TraceEnd = EyeStart + (ShotDirection * 10000);
 
+	//Найти конечную точку трейсинга
+	FVector TraceEnd = EyeStart + (ShotDirection * LenghtTracing);
+
+	//Параметры коллизии
 	FCollisionQueryParams QueryParams;
+
+	//Игнорировать Овнера
 	QueryParams.AddIgnoredActor(MyOwner);
+
+	//Игнорировать себя
 	QueryParams.AddIgnoredActor(this);
+
+	//Трейсить комплексную коллизию (собственную коллизию меша)
 	QueryParams.bTraceComplex = true;
+
+	//Возвращать физический материал при результате
 	QueryParams.bReturnPhysicalMaterial = true;
 
+	//Запомнить для изменения
 	FVector TracerEndPoint = TraceEnd;
 
+	//Определить физ. материал по умолчанию
 	EPhysicalSurface SurfaceType = SurfaceType_Default;
 
+	//Трейсинг, если удался
 	if (GetWorld()->LineTraceSingleByChannel(Hit, EyeStart, TraceEnd, COLLISION_WEAPON, QueryParams))
 	{
+		//Получить попавшегося актора
 		AActor* HitActor = Hit.GetActor();
 
+		//Получить физ. материал 
 		SurfaceType = UPhysicalMaterial::DetermineSurfaceType(Hit.PhysMaterial.Get());
+
+		//Текущий урон
 		float CurrentDamage = BaseDamage;
+
+		//Если заданный материал, то увеличить урон (хедшот)
 		if (SurfaceType == SURFACE_FLESHVULNERABLE)
 		{
 			CurrentDamage *= 4.0f;
 		}
 
+		//Применить урон к HitActor
 		UGameplayStatics::ApplyPointDamage(HitActor, CurrentDamage, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
 
+		//Получить текущую точку трейсинга
 		TracerEndPoint = Hit.ImpactPoint;
 
 		//Показать эффекты от выстрела на поверхности
 		PlayImpactEffects(SurfaceType, TracerEndPoint);
 	}
 
+	//Если задано в консоли, отрисовать дебаг-линию
 	if (DebugWeaponDrawing > 0)
 	{
 		DrawDebugLine(GetWorld(), EyeStart, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
@@ -105,13 +145,14 @@ void ASWeapon::Fire()
 	//Показать эффекты от выстрела на оружии
 	PlayFireEffects(TracerEndPoint);
 
-	//Если это сервер, заполнить структуру для передачи на него
+	//Если это сервер, заполнить структуру для передачи по клиентам
 	if (Role == ROLE_Authority)
 	{
 		HitScanTrace.TraceTo = TracerEndPoint;
 		HitScanTrace.SurfaceType = SurfaceType;
 	}
 
+	//Запомнить время последнего выстрела
 	LastFireTime = GetWorld()->TimeSeconds;
 }
 
@@ -121,7 +162,7 @@ void ASWeapon::ServerFire_Implementation()
 	Fire();
 }
 
-//Проверять доступность сервера?
+//Выполнять на сервере?
 bool ASWeapon::ServerFire_Validate()
 {
 	return true;
@@ -139,33 +180,48 @@ void ASWeapon::OnRep_HitScanTrace()
 
 void ASWeapon::StartFire()
 {
-	float FirstDelay = FMath::Max(LastFireTime + TimeBetweenShots - GetWorld()->TimeSeconds, 0.0f);
+	/*
+	Рассчитать задержку перед работой таймера, ограничить результат >=0.0f. Если перерыв между стрельбой (LastFireTime - GetWorld()->TimeSeconds)
+	больше, чем время между выстрелами TimeBetweenShots, то задержка = 0. 
+	Если перерыв меньше, то будет небольшая задержка больше 0 и меньше, чем TimeBetweenShots
+	*/
+	float FirstDelay = FMath::Max( TimeBetweenShots + LastFireTime - GetWorld()->TimeSeconds, 0.0f);
+
+	//Запустить таймер, по которому выполняется Fire() - стрельба
 	GetWorldTimerManager().SetTimer(TimerHandle_TimeBetweenShots, this, &ASWeapon::Fire, TimeBetweenShots, true, FirstDelay);
 }
 
 void ASWeapon::StopFire()
 {
+	//Удалить таймер
 	GetWorldTimerManager().ClearTimer(TimerHandle_TimeBetweenShots);
 }
 
 void ASWeapon::PlayFireEffects(FVector TracerEndPoint)
 {
+	//Спавнить приатаченные к MeshComp частицы в сокете
 	if (MuzzleEffect)
 	{
 		UGameplayStatics::SpawnEmitterAttached(MuzzleEffect, MeshComp, MuzzleSocketName);
 	}
 
+	//Спавнить частицы эффекта трейсинга
 	if (TracerEffect)
 	{
+		//Взять точку сокета
 		FVector MuzzleSocketLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+
+		//Спавнить частицы в сокете
 		UParticleSystemComponent* TracerComp = UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), TracerEffect, MuzzleSocketLocation);
 
 		if (TracerComp)
 		{
+			//Вытянуть частицы до точки конца трейсинга
 			TracerComp->SetVectorParameter(TracerTargetName, TracerEndPoint);
 		}
 	}
 
+	//Создать эффект тряски камеры
 	APawn* MyOwner = Cast<APawn>(GetOwner());
 	if (MyOwner)
 	{
@@ -179,9 +235,10 @@ void ASWeapon::PlayFireEffects(FVector TracerEndPoint)
 
 void ASWeapon::PlayImpactEffects(EPhysicalSurface SurfaceType, FVector ImpactPoint)
 {
+	//Объявить частицы
 	UParticleSystem* SelectImpact = nullptr;
 
-//Задать частицы в зависимости от материала поверхности
+//Задать частицы в зависимости от физ. материала поверхности
 	switch (SurfaceType)
 	{
 	case SUFRACE_FLESHDEFAULT:
@@ -197,19 +254,26 @@ void ASWeapon::PlayImpactEffects(EPhysicalSurface SurfaceType, FVector ImpactPoi
 	{
 		//Положение сокета на оружии
 		FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+
 		//Вектор, направленный в сторону стрельбы
 		FVector ShotVectorDirection = ImpactPoint - MuzzleLocation;
+
 		ShotVectorDirection.Normalize();
-		//Ротатор в сторону стрельбы
+
+		//Получить ротатор в сторону стрельбы
 		FRotator ShotRotatorDirection = ShotVectorDirection.Rotation();
+
 		//Спавн частиц на поверхности 
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), SelectImpact, ImpactPoint, ShotRotatorDirection);
 	}
 
 }
 
+//Реплицировать переменные на клиенты
 void ASWeapon::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Реплицировать на всех, кроме клиента-овнера оружия
 	DOREPLIFETIME_CONDITION(ASWeapon, HitScanTrace, COND_SkipOwner);
 }
--- Source/CoopGame/Public/SWeapon.h
+++ Source/CoopGame/Public/SWeapon.h
@@ -13,7 +13,7 @@ class UDamageType;
 class UParticleSystem;
 class UCameraShake;
 
-//Структура для хранения данных трейсинга, передаваемых в сети.
+//Структура для хранения данных трейсинга, которые реплицируются
 USTRUCT()
 struct FHitScanTrace
 {
@@ -41,55 +41,90 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	//Меш
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 		USkeletalMeshComponent* MeshComp;
 
+	//Класс компонента для описания повреждений - задается в редакторе как тип повреждений
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		TSubclassOf<UDamageType> DamageType;
+
+	//Название сокета на оружии
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		FName MuzzleSocketName;
+
+	//Имя параметра частиц эффекта трейсинга при выстреле TracerEffect
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		FName TracerTargetName;
+
+	//Частицы эффекта выстрела на оружии
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		UParticleSystem* MuzzleEffect;
+
+	//Дефолтные эффекты выстрела на поверхности
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		UParticleSystem* DefaultImpactEffect;
+
+	//Эффекты выстрела на поверхности с определенным материалом
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		UParticleSystem* FleshImpactEffect;
+
+	//Эффект трейсинга при выстреле - линия по лучу трейсинга
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
 		UParticleSystem* TracerEffect;
+
+	//Длина трейсинга
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+		float LenghtTracing;
+
+	//Базовый дамаг
 	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
 		float BaseDamage;
 
-	//Показать эффекты от выстрела на оружии
+	//Показать эффекты от выстрела
 	void PlayFireEffects(FVector TracerEndPoint);
 
 	//Показать эффекты от выстрела на поверхности
 	void PlayImpactEffects(EPhysicalSurface SurfaceType, FVector ImpactPoint);
 
+	//Тряска камеры при выстреле
 	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
 		TSubclassOf<UCameraShake> FireCamShake;
 
+	//Выстрел
 	virtual void Fire();
 
+	//Выстрел на сервере
 	UFUNCTION(Server, Reliable, WithValidation)
 		void ServerFire();
 
+	//Таймер для автоматического выстрела
 	FTimerHandle TimerHandle_TimeBetweenShots;
+
+	//Время последнего выстрела
 	float LastFireTime;
 
-	/*RPM - Bullets per minute fired by weapon*/
+	//Частота выстрела - количество пуль в 1 минуту
 	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
 		float RateOfFire;
+
+	//Время между выстрелами
 	float TimeBetweenShots;
 
+	//Структура для репликации 
 	UPROPERTY(ReplicatedUsing = OnRep_HitScanTrace)
 		FHitScanTrace HitScanTrace;
+
+	//Выполняется на клиенте, когда HitScanTrace реплицируется 
 	UFUNCTION()
 		void OnRep_HitScanTrace();
 
 public:	
+
+	//Начать стрельбу
 	void StartFire();
+
+	//Закончить стрельбу
 	void StopFire();
 
 };
